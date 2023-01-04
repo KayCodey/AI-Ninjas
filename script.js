08d25fad17296e7c91ea685668de972863003cd4
Katarzyna Wiltos <42550922+KayCodey@users.noreply.github.com>
2023-01-04 01:04:12 +0100

    Add three levels of accuracy
    low/medium/high risk of provided link to be a phishing attempt

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 var accuracy = -1;
 // Some more code needed to provide calculated accuracy form python script ...
 // Condition to change color of the feedback box (green / red)
-if (accuracy >=0 &&  accuracy <= 50) {
+if (accuracy >=0 &&  accuracy <= 30) {
     document.getElementById("output").innerHTML = "there is "+accuracy+"% of chance that provided link is a phishing attempt";
     document.getElementById("phishing-prediction").style.backgroundColor = "#77C676";
-} else if (accuracy > 50 && accuracy <= 100) {
+} else if (accuracy > 30 && accuracy < 60) {
+    document.getElementById("output").innerHTML = "there is "+accuracy+"% of chance that provided link is a phishing attempt";
+    document.getElementById("phishing-prediction").style.backgroundColor = "#DD8D53";
+} else if (accuracy >= 60 && accuracy <= 100) {
     document.getElementById("output").innerHTML = "there is "+accuracy+"% of chance that provided link is a phishing attempt";
     document.getElementById("phishing-prediction").style.backgroundColor = "#E04E4E";
 }
