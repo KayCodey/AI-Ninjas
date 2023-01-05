8c0a3c93b5e7560d610d514c827ad67a1cb84acb
Paulina <paulina30405@gmail.com>
2023-01-05 17:35:22 +0100

    Setup a connection with the server.

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
 // You can test visuals by manually changing accuracy value
-var accuracy = -1;
+var accuracy = 100;
 // Some more code needed to provide calculated accuracy form python script ...
 // Condition to change color of the feedback box (green / red)
-if (accuracy >=0 &&  accuracy <= 30) {
-    document.getElementById("output").innerHTML = "there is "+accuracy+"% of chance that provided link is a phishing attempt";
-    document.getElementById("phishing-prediction").style.backgroundColor = "#77C676";
-} else if (accuracy > 30 && accuracy < 60) {
-    document.getElementById("output").innerHTML = "there is "+accuracy+"% of chance that provided link is a phishing attempt";
-    document.getElementById("phishing-prediction").style.backgroundColor = "#DD8D53";
-} else if (accuracy >= 60 && accuracy <= 100) {
-    document.getElementById("output").innerHTML = "there is "+accuracy+"% of chance that provided link is a phishing attempt";
-    document.getElementById("phishing-prediction").style.backgroundColor = "#E04E4E";
+
+var outputParagraph  = document.getElementById("output")
+var backgroundDiv =  document.getElementById("phishing-prediction")
+var button = document.getElementById("checkButton")
+var linkInput = document.getElementById("link-to-be-checked")
+button.addEventListener("click", HandleLinkCheck);
+
+
+function HandleLinkCheck()
+{
+    console.log(linkInput.value)
+    let link = linkInput.value
+
+    GetAccuracy(link)
 }
+
+function GetAccuracy(link)
+{
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function()
+    {
+        if (this.readyState == 4 && this.status == 200)
+        {
+            if (this.responseText !== "")
+            {
+                //TO DO assign proper accuracy to variable
+                let acc = parseInt(this.responseText)
+                SetAccuracy(acc)
+            }
+        }
+    }
+
+    xmlhttp.open("POST", "readLines.php", true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(link);
+
+}
+
+function SetAccuracy(a){
+    accuracy = a
+
+    if (accuracy >=0 &&  accuracy <= 30) {
+        outputParagraph.innerHTML = "there is "+accuracy+"% of chance that provided link is a phishing attempt";
+        backgroundDiv.style.backgroundColor = "#77C676";
+    } else if (accuracy > 30 && accuracy < 60) {
+        outputParagraph.innerHTML = "there is "+accuracy+"% of chance that provided link is a phishing attempt";
+        backgroundDiv.style.backgroundColor = "#DD8D53";
+    } else if (accuracy >= 60 && accuracy <= 100) {
+        outputParagraph.innerHTML = "there is "+accuracy+"% of chance that provided link is a phishing attempt";
+        backgroundDiv.style.backgroundColor = "#E04E4E";
+    }
+}
+
