f2189168a095b9bb721e398fef77efaa0d4f1a9b
Paulina <paulina30405@gmail.com>
2023-01-15 20:53:34 +0100

    Now a request to server works.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // You can test visuals by manually changing accuracy value
-var accuracy = 100;
+var accuracy = 85;
 // Some more code needed to provide calculated accuracy form python script ...
 // Condition to change color of the feedback box (green / red)
 
@@ -9,6 +9,11 @@ var button = document.getElementById("checkButton")
 var linkInput = document.getElementById("link-to-be-checked")
 button.addEventListener("click", HandleLinkCheck);
 
+class Link{
+    constructor(link) {
+        this.url = link
+    }
+}
 
 function HandleLinkCheck()
 {
@@ -20,7 +25,10 @@ function HandleLinkCheck()
 
 function GetAccuracy(link)
 {
+    var linkObj = new Link(link)
     var xmlhttp = new XMLHttpRequest();
+
+    const json = JSON.stringify(linkObj);
 
     xmlhttp.onreadystatechange = function()
     {
@@ -28,17 +36,21 @@ function GetAccuracy(link)
         {
             if (this.responseText !== "")
             {
-                //TO DO assign proper accuracy to variable
-                let acc = parseInt(this.responseText)
-                SetAccuracy(acc)
+                let response = this.responseText
+
+                if (response == "True"){
+                    SetText(true)
+                }
+                else{
+                    SetText(false)
+                }
             }
         }
     }
 
-    xmlhttp.open("POST", "readLines.php", true);
+    xmlhttp.open("POST", "http://130.162.38.93:20421/check", true);
     xmlhttp.setRequestHeader("Content-Type", "application/json");
-    xmlhttp.send(link);
-
+    xmlhttp.send(json);
 }
 
 function SetAccuracy(a){
@@ -56,3 +68,13 @@ function SetAccuracy(a){
     }
 }
 
+function SetText(isPhisingAttempt){
+    if (isPhisingAttempt){
+        outputParagraph.innerHTML =  "there is "+accuracy+"% of chance that provided link is a phishing attempt";
+        backgroundDiv.style.backgroundColor = "#E04E4E";
+    }
+    else{
+        outputParagraph.innerHTML =  "there is "+accuracy+"% of chance that provided link is not a phishing attempt";
+        backgroundDiv.style.backgroundColor = "#77C676";
+    }
+}
